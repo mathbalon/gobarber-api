a0dfecfbc3c347bcb048e8eccd57a2d7fbe368a5
Matheus Balonecker <mathbalon@gmail.com>
2021-02-14 17:40:26 -0400

    correção da escrita das variáveis

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -7,9 +7,9 @@ export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id
 
-    const listprovider = container.resolve(ListProvidersService)
+    const listProviders = container.resolve(ListProvidersService)
 
-    const providers = await listprovider.execute({
+    const providers = await listProviders.execute({
       user_id,
     })
 
