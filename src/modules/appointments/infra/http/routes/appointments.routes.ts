3748d58d20d9fde93be96f3196f77795934507fc
Matheus Balonecker <mathbalon@gmail.com>
2021-02-14 20:09:18 -0400

    validação das rotas - celebrate

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import AppointmentsController from '../controllers/AppointmentsController'
@@ -11,7 +12,16 @@ const providerAppointmentsController = new ProviderAppointmentsController()
 
 appointementsRouter.use(ensureAuthenticated)
 
-appointementsRouter.post('/', appointmentsController.create)
+appointementsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+)
 
 appointementsRouter.get(
   '/provider-schedule',
--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 import ProfileController from '../controllers/ProfileController'
@@ -10,6 +11,24 @@ profileRouter.use(ensureAuthenticated)
 
 profileRouter.get('/', profileControle.show)
 
-profileRouter.put('/', profileControle.update)
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('password')).required(),
+      }),
+    },
+  }),
+  profileControle.update,
+)
 
 export default profileRouter
