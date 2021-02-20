813d8688ec02f87fc4d442e7cf3a830696178825
Matheus Balonecker <mathbalon@gmail.com>
2021-02-20 14:30:47 -0400

    fix: remove sensitive information

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe'
+import { classToClass } from 'class-transformer'
 
 import IAppointmentRepository from '../repositories/IAppointmentRepository'
 
@@ -43,7 +44,7 @@ export default class ListProviderAppointmentsService {
       )
     }
 
-    await this.cacheProvider.save(cacheKey, appointments)
+    await this.cacheProvider.save(cacheKey, classToClass(appointments))
 
     return appointments
   }
